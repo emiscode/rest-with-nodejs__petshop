d9bc4f934fd12051b040f3eddece907c1a429f6c
Emilio Scodeler <emilios@ciandt.com>
2021-09-16 16:56:26 -0300

    Add file validation

--- src/files/uploadFile.js
+++ src/files/uploadFile.js
@@ -2,10 +2,26 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = (imgPath, imgName, callBackFileCreated) => {
+    const fileTypes = ['jpg', 'jpeg', 'png']
     const extension = path.extname(imgPath)
-    const storagePath = `./src/assets/img/storage/${imgName}${extension}`
+    const isValidType = fileTypes.indexOf(extension.substring(1)) !== -1
 
-    fs.createReadStream(imgPath)
-        .pipe(fs.createWriteStream(storagePath))
-        .on('finish', () => callBackFileCreated(storagePath))
+    if (isValidType) {
+        const storagePath = `./src/assets/img/storage/${imgName}${extension}`
+
+        if (fs.existsSync(imgPath)) {
+            fs.createReadStream(imgPath)
+                .pipe(fs.createWriteStream(storagePath))
+                .on('finish', () => callBackFileCreated(false, storagePath))
+        } else {
+            const err = `File not found ${imgPath}`
+            console.log(err)
+            callBackFileCreated(err)
+        }
+
+    } else {
+        const err = `Invalid file type ${extension}`
+        console.log(err)
+        callBackFileCreated(err)
+    }
 }
--- src/models/Pet.js
+++ src/models/Pet.js
@@ -7,20 +7,24 @@ class Pet {
     constructor() {
         this.tableName = 'pets'
     }
-    
+
     create(pet, res) {
         const sql = `INSERT INTO ${this.tableName} set ?`
 
-        uploadFile(pet.imagem, pet.nome, (storagePath) => {
-            const newPet = { nome: pet.nome, imagem: storagePath }
-            
-            db.query(sql, newPet, (err, data) => {
-                if (!err) {
-                    const petCadastrado = { id: data.insertId, ...newPet } 
-                    return res.status(201).json(petCadastrado)
-                }
-                else return res.status(400).json(err)
-            })
+        uploadFile(pet.imagem, pet.nome, (err, storagePath) => {
+            if (err) {
+                res.status(400).json(err)
+            } else {
+                const newPet = { nome: pet.nome, imagem: storagePath }
+
+                db.query(sql, newPet, (err, data) => {
+                    if (!err) {
+                        const petCadastrado = { id: data.insertId, ...newPet }
+                        return res.status(201).json(petCadastrado)
+                    }
+                    else return res.status(400).json(err)
+                })
+            }
         })
     }
 }
